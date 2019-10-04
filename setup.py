02098b538ae9449e489763d56bd238a511c460fa
Sumeet <sumeet.kumar507@gmail.com>
2019-10-04 16:36:45 -0700

    Changed Classifier in Setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DAQdata",
+    name="DAQData",
     version="2.0",
     author="Sumeet Kumar Sinha",
     author_email="sumeet.kumar507@gmail.com",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/SumeetSinha/DAQdata",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Intended Audience :: Centrifuge Research at Center of Geotechnical Modeling at UC Davis",
+        "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
@@ -30,7 +30,3 @@ setuptools.setup(
     py_modules=["DAQdata"],
 )
 
-
-# python -m pip install --user --upgrade setuptools wheel
-
-# python setup.py sdist bdist_wheel
