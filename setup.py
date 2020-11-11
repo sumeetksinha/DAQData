76a8028d5e683d1169a0dd46bf554da0555944da
Sumeet <sumeet.kumar507@gmail.com>
2020-11-11 11:48:26 -0800

    Added DAQ_Data Jupyter Notebook app

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DAQData",
-    version="2.4",
+    version="2.5",
     author="Sumeet Kumar Sinha",
     author_email="sumeet.kumar507@gmail.com",
     description="Read and plot slow and fast data binary files from centrifuge experiments conducted at Center of Geotechnical Modeling at University of California Davis",
@@ -25,7 +25,7 @@ setuptools.setup(
     
     keywords='Centrifuge, Center of Geotechnical MOdeling, CGM, UC Davis, Binary Data',
 
-    install_requires=['matplotlib','pandas','StringIO'],
+    install_requires=['matplotlib','pandas'],
 
     py_modules=["DAQData"],
 )
